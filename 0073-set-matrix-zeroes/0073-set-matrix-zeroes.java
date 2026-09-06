48ebb617ed814e9d8ebf6e9c3cce31d1fe289b8c
abhishek kuntal <abhikuntal8267@gmail.com>
2026-09-06 16:41:43 +0530

    Time: 1 ms (96.86%), Space: 47.7 MB (42.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,17 +1,17 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        boolean fstRow = false;
-        boolean fstCol= false;
 
-        for(int j=0; j<arr[0].length; j++){
+        boolean fstrow = false;
+        boolean fstcol = false;
+
+        for(int j =0; j<arr[0].length; j++){
             if(arr[0][j] == 0){
-                fstRow = true;
+                fstrow = true;
             }
         }
-
-         for(int i=0; i<arr.length; i++){
+         for(int i =0; i<arr.length; i++){
             if(arr[i][0] == 0){
-                fstCol = true;
+                fstcol = true;
             }
         }
         
@@ -19,11 +19,11 @@ class Solution {
             for(int j=1; j<arr[0].length; j++){
                 if(arr[i][j] == 0){
                     arr[i][0] = 0;
-                    arr[0][j] =0;
+                    arr[0][j] = 0;
                 }
-
             }
         }
+
         for(int i=1; i<arr.length; i++){
             for(int j=1; j<arr[0].length; j++){
                 if(arr[i][0]==0 || arr[0][j]==0){
@@ -32,16 +32,16 @@ class Solution {
             }
         }
 
-        if(fstRow){
+        if(fstrow){
             for(int j=0; j<arr[0].length; j++){
                 arr[0][j] = 0;
             }
         }
-         if(fstCol){
+
+         if(fstcol){
             for(int i=0; i<arr.length; i++){
                 arr[i][0] = 0;
             }
         }
-
     }
 }
